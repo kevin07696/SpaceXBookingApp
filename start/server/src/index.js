1b03f29bba5e4623cd2cbca6240dc44d510ef77d
Kevin Lam <kevinlam07696@gmail.com>
2021-08-02 00:59:38 -0400

    Connected to SpaceX v2 REST API and Sql Database

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -1,13 +1,28 @@
 const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema');
 
-const server = new ApolloServer({ typeDefs });
+const { createStore } = require('./utils');
+
+
+const LaunchAPI = require('./datasources/launch');
+const UserAPI = require('./datasources/user');
+
+
+const store = createStore();
+
+const server = new ApolloServer({
+  typeDefs,
+
+  dataSources: () => ({
+    launchAPI: new LaunchAPI(),
+    userAPI: new UserAPI({ store })
+  })
+});
 
 server.listen().then(() => {
-    console.log(`
-      Server is running!
-      Listening on port 4000
-      Explore at https://studio.apollographql.com/sandbox
-    `);
-  });
-  
+  console.log(`
+    Server is running!
+    Listening on port 4000
+    Explore at https://studio.apollographql.com/sandbox
+  `);
+});
